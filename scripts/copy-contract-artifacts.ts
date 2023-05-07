c09a0ff3b0d2b9fd2279fecd08ffe7f3a8918a11
abbylow <yu950713@gmail.com>
2023-05-08 01:09:40 +0800

    Update copy artifact script and abi

--- scripts/copy-contract-artifacts.ts
+++ scripts/copy-contract-artifacts.ts
@@ -1,10 +1,10 @@
 import * as fs from "fs"
 
 async function main() {
-    const contractArtifactsPath = "apps/contracts/build/contracts/contracts/Feedback.sol"
+    const contractArtifactsPath = "apps/contracts/build/contracts/contracts/Post.sol"
     const webAppArtifactsPath = "apps/web-app/contract-artifacts"
 
-    await fs.promises.copyFile(`${contractArtifactsPath}/Feedback.json`, `${webAppArtifactsPath}/Feedback.json`)
+    await fs.promises.copyFile(`${contractArtifactsPath}/Post.json`, `${webAppArtifactsPath}/Post.json`)
 }
 
 main()
