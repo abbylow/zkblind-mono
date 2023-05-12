5cac0e44b553d053995fa4b48baf8b159318fcc6
abbylow <yu950713@gmail.com>
2023-05-12 11:08:38 +0800

    Change the arweave host

--- apps/web-app/src/components/PostCard.tsx
+++ apps/web-app/src/components/PostCard.tsx
@@ -9,7 +9,11 @@ interface IPostCard {
   arHashedId: string
 }
 
-const arweave = Arweave.init({});
+const arweave = Arweave.init({
+  host: "arweave.net", // Hostname or IP address for a Arweave host
+  port: 443, // Port
+  protocol: "https" // Network protocol http or https
+})
 
 const PostCard: React.FC<IPostCard> = ({
   group = '',
